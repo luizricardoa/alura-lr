589aaa7701badb177aa5ccaf6720054ab5075260
luizricardoa <luizricardoaraujo@hotmail.com>
2022-06-15 14:20:26 -0300

    atualizacoes js.v2

---
+++ Alura/JS-Backend/conversoes.js
@@ -0,0 +1,10 @@
+// tipo de dado com booleanos
+
+// conversao implicita => numero em string ou vice-versa
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);
+
+
+// conversao explicita => 
---
+++ Alura/JS-Backend/truthy-falsy.js
@@ -0,0 +1,26 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false);
+// console.log("" == false);
+// console.log(1 == true);
+// console.log(1 == false);
+
+
+// null => vazio
+
+
+// undefined
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
