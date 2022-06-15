c7b2a4a88042140d30156420867c63044afe4f83
luizricardoa <luizricardoaraujo@hotmail.com>
2022-06-15 15:36:52 -0300

    conversoes.js finalizado

--- Alura/JS-Backend/conversoes.js
+++ Alura/JS-Backend/conversoes.js
@@ -4,7 +4,24 @@
 const numero = 456;
 const numeroString = "456";
 
-console.log(numero == numeroString);
-
+//console.log(numero + numeroString);
 
 // conversao explicita => 
+
+// Number()
+// String()
+
+//console.log(numero + Number(numeroString));
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
