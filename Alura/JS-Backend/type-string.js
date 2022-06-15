523a095183172ca7c833d6ba79e8fa1b6e275b5b
Luiz Ricardo Araujo <you@example.com>
2022-06-14 21:16:35 -0300

    aula de Backend

---
+++ Alura/JS-Backend/type-string.js
@@ -0,0 +1,31 @@
+const texto1 = "Ola, Mundo!";
+const texto2 = 'Ola, Mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nom eh ";
+const meuNome = "Luiz";
+
+// concatenacao (+)
+
+//console.log(citacao + meuNome);
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+//console.log(cifrao)
+//console.log(aMaiusculo)
+//console.log(tique)
+//console.log(hiragana)
+
+
+
+//template string ou template literal
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
